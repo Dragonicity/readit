cf15073165489a164a87b08db85537284460d076
Mark-Nelson <mark.nelson@dragonicity.com>
2016-11-03 10:02:07 +0800

    Add counter_cache for story votes

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102070624) do
+ActiveRecord::Schema.define(version: 20161103014614) do
 
   create_table "stories", force: :cascade do |t|
     t.string   "name"
     t.string   "link"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.integer  "votes_count"
   end
 
   create_table "users", force: :cascade do |t|
